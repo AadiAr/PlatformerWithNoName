cf8a9e913292d29e6a6a02c2beb431bb4a5550cd
AadiAr <84633192+AadiAr@users.noreply.github.com>
2021-05-27 14:52:18 +0530

    Update RenderWindow.cpp

--- Code/RenderWindow.cpp
+++ Code/RenderWindow.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL_image.h>
 
 #include <iostream>
-
+#include "Entity.hpp"
 #include "RenderWindow.hpp"
 
 using namespace std;
@@ -12,7 +12,7 @@ RenderWindow::RenderWindow(const char* title, int width, int height)
 {
     window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     if(window == NULL)
     {
@@ -35,9 +35,20 @@ void RenderWindow::Clear()
 {
     SDL_RenderClear(renderer);
 }
-void RenderWindow::Render(SDL_Texture* tex)
+void RenderWindow::Render(Entity& entity)
 {
-    SDL_RenderCopy(renderer,tex,NULL,NULL);
+    SDL_Rect src;
+    src.x = entity.getCurrentFrame().x;
+    src.y = entity.getCurrentFrame().y;
+    src.w = entity.getCurrentFrame().w;
+    src.h = entity.getCurrentFrame().h;
+
+    SDL_Rect dst;
+    dst.x = entity.getPos().x * 4;
+    dst.y = entity.getPos().y * 4;
+    dst.w = entity.getCurrentFrame().w * 4;
+    dst.h = entity.getCurrentFrame().h * 4;
+    SDL_RenderCopy(renderer,entity.getTex(),&src,&dst);
 }
 void RenderWindow::Display()
 {
